92b6b33f0c128b9d3da12721d8fadaa2c59f254a
sri_satya <psatyachowdary2001@gmail.com>
2026-03-19 10:48:29 +0530

    First commit

---
+++ src/main/java/com/hanuman/webapp/controller/AppController.java
@@ -0,0 +1,24 @@
+package com.hanuman.webapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+    @GetMapping()
+    @ResponseBody
+    public String welcomeMessage(){
+        return "Welcome to webapp";
+    }
+
+    @GetMapping("/deploy")
+    public ResponseEntity<String> testController(){
+        return ResponseEntity.ok("This is webapp if your are seeing this message means application deployment is successful");
+
+    }
+}
