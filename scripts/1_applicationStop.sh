4c216a9e0be3d16f91f460450c0e52890693cd99
sri_satya <psatyachowdary2001@gmail.com>
2026-03-20 15:24:48 +0530

    intentionally make a typo in 1_applicationStop.sh script

--- scripts/1_applicationStop.sh
+++ scripts/1_applicationStop.sh
@@ -2,12 +2,14 @@
 
 echo "Stopping application"
 
-sh /opt/dev/apache-tomcat-11.0.18/bin/catalina.sh stop
+sd /opt/dev/apache-tomcat-11.0.18/bin/catalina.sh stop
 
 sleep 1
 
 if [[ ! -n $(pgrep java) ]]; then
 	echo "Application stopped successfully"
 fi
+
+exit 1
 
 
